dd468ca7cd6fb70162bda4549ee86b31405fe75a
BrunoCalace <bruno.calace@gmail.com>
2024-08-16 18:55:58 -0300

    0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/home.jsx'
+import ErrorPage from '../src/pages/Error/Error.jsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={ <Home /> } />
+          <Route path="*" element={ <ErrorPage /> } />
         </Routes>
       </Router>
     </main>
