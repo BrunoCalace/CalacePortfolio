265d0def448cb5f5bdd035e0f15e853904e3349f
BrunoCalace <bruno.calace@gmail.com>
2024-08-19 20:14:29 -0300

    0.0.6

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,18 @@
 import './styles.css'
+import cv from '../../assets/docs/CalaceBruno-CV.pdf'
 
 function About() {
     return(
-        <div className='bg'>
+        <div className='aboutBox'>
+            <h1>Bruno Calace</h1>
+            <h2>Bienvenido a mi portfolio</h2>
+            <p>Soy desarrollador fullstack, apasionado de la tecnología y el deporte.</p>
+            <div className='aboutDestContainer'>
+                <p className='aboutDest'>Actualmente estoy estudiando la carrera de Ingeniería en Informática en la Universidad Nacional de La Matanza y estoy en búsqueda de experiencias laborales que me permitan desarrollar y aplicar los conocimientos que adquiero día a día en la carrera.</p>
+            </div>
+            <a href={cv} download="CalaceBruno-CV.pdf">
+                <button>Descargar CV</button>
+            </a>
             
         </div>
     );
