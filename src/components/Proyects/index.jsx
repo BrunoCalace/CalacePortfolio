356b89e9461bd18cfbb7ef1ad1c3a936b7f51c5f
BrunoCalace <bruno.calace@gmail.com>
2024-08-20 23:21:38 -0300

    0.0.9

--- src/components/Proyects/index.jsx
+++ src/components/Proyects/index.jsx
@@ -4,13 +4,19 @@ import Css from '../Skills/Skillcss'
 import Javascript from '../Skills/Skilljs'
 import React from '../Skills/Skillreact'
 import Bootstrap from '../Skills/Skillbs'
+import Nodejs from '../Skills/Skillnodejs'
+import Mongo from '../Skills/Skillmongo'
+import Express from '../Skills/Skillex'
 
 function Proyects() {
     return(
         <div className='proyectBox'>
             <div className='proyectCard'>
-                <img src="#" alt="proyect image" />
+                <div className='proyectTitle'>
+                    <h1>Ecommerce Frontend</h1>
+                </div>
                 <div className='proyectDesc'>
+                    <p>En este proyecto se utilizo: </p>
                     <ul>
                         <li><Html /></li>
                         <li><Css /></li>
@@ -18,7 +24,25 @@ function Proyects() {
                         <li><React /></li>
                         <li><Bootstrap /></li>
                     </ul>
-                    <a href="#"></a>
+                    <a href="#">deploy</a>
+                </div>
+            </div>
+            <div className='proyectCard'>
+                <div className='proyectTitle'>
+                    <h1>Ecommerce Fullstack</h1>
+                </div>
+                <div className='proyectDesc'>
+                    <p>En este proyecto se utilizo: </p>
+                    <ul>
+                        <li><Html /></li>
+                        <li><Css /></li>
+                        <li><Javascript /></li>
+                        <li><Bootstrap /></li>
+                        <li><Nodejs /></li>
+                        <li><Mongo /></li>
+                        <li><Express /></li>
+                    </ul>
+                    <a href="#">deploy</a>
                 </div>
             </div>
         </div>
