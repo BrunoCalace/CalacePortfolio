00bdae62dbad03710ae995e92b4e9631e1d33007
BrunoCalace <bruno.calace@gmail.com>
2024-09-10 15:17:45 -0300

    Portfolio 1.0.1

--- src/components/Proyects/index.jsx
+++ src/components/Proyects/index.jsx
@@ -1,6 +1,7 @@
 import './styles.css'
 import Html from '../Skills/Skillhtml'
 import Css from '../Skills/Skillcss'
+import Sass from '../Skills/Skillsass'
 import Javascript from '../Skills/Skilljs'
 import React from '../Skills/Skillreact'
 import Bootstrap from '../Skills/Skillbs'
@@ -11,6 +12,22 @@ import Express from '../Skills/Skillex'
 function Proyects() {
     return(
         <div className='proyectBox'>
+            <div className='proyectCard'>
+                <div className='proyectTitle' id='C'>
+                    <h1>Pathfinder Spell List</h1>
+                </div>
+                <div className='proyectDesc'>
+                    <p>Este es un proyecto sin fin de lucro, todos los derechos reservados Paizo "Pathfinder: 2nd edition". Spell list y spell repertoire con spell slots atomatizados para cada lvl. La idea surge de optimizar el tiempo de decisión sobre que poder utilizar por los spell casters asi como el facil acceso a su información durante una sesión de TTRPG.</p>
+                    <p>En este proyecto se utilizo: </p>
+                    <ul>
+                        <li><Html /></li>
+                        <li><Sass /></li>
+                        <li><Javascript /></li>
+                        <li><React /></li>
+                    </ul>
+                    <a href="https://brunocalace.github.io/occultSpellList/">brunocalace.github.io/occultSpellList/</a>
+                </div>
+            </div>
             <div className='proyectCard'>
                 <div className='proyectTitle' id='A'>
                     <h1>Ecommerce Frontend</h1>
